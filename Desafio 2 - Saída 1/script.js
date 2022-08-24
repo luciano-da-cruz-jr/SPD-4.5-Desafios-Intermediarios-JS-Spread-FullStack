626842d70dd41f996298a30a284d60e74dbc7a57
Luciano da Cruz Junior <lucianocruzjunior@gmail.com>
2022-08-24 17:06:22 -0300

    Adicionando a resposta do segundo desafio - duas soluções diferentes

---
+++ Desafio 2 - Saída 1/script.js
@@ -0,0 +1,65 @@
+//Desafio 2 - Saída 1
+
+//Solucção 1
+
+/*
+let frase;
+let cont = 0;
+let cont2 = 1;
+
+do 
+{
+    frase = "";
+    cont = cont + 1;  
+
+    for(let i=1; i<40; i++)
+    {       
+        frase = frase + "-";
+    }
+
+    console.log(frase);
+
+    while (cont2 <= 5)
+    {
+        cont2 = cont2 + 1;
+
+        frase = "";
+
+        for(let i=1; i<40; i++)
+        {
+            if(i == 1 || i == 39)
+            {
+                frase = frase + "|";
+            }else
+            {
+                frase = frase + " ";
+            }
+        }
+
+        console.log(frase);
+    }
+  
+} while (cont < 2);
+
+
+//Solucção 2
+
+let cont = 0;
+let cont2 = 1;
+
+do 
+{
+    cont = cont + 1; 
+
+    console.log("---------------------------------------");
+
+    while (cont2 <= 5)
+    {
+        cont2 = cont2 + 1;
+
+        console.log("| |");
+    }
+  
+} while (cont < 2);
+
+*/
